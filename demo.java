b54efa78adc10c2458bebd32bc5536c318ba6d13
toufique776 <toufiq.nt@gmail.com>
2025-01-23 23:04:55 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,7 @@
+// Simple Java Program
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+    }
+}
