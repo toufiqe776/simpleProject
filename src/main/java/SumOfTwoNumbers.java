6e85583278d3dba456d5708c51d53dafb1563824
toufiq.nt@gmail.com <toufiq.nt@gmail.com>
2025-04-03 19:56:13 +0530

    sum

--- src/main/java/SumOfTwoNumbers.java
+++ src/main/java/SumOfTwoNumbers.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class SumOfTwoNumbers {
+    public static void main(String []args){
+        int a=10;
+        int b=90;
+        System.out.println("Sum Of A and B:"+(a+b));
+    }
 }
